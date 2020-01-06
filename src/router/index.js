ffc68b925aaff7eca08408cebe93ac0cbda33d65
YangK <1041502459@qq.com>
2020-01-06 19:18:59 +0800

    验证登录

--- src/router/index.js
+++ src/router/index.js
@@ -13,3 +13,15 @@ const router = new VueRouter({
 })
 
 export default router
+
+// 配置全局路由守卫验证登录状态
+router.beforeEach((to, from, next) => {
+  // 获取用户的登录信息
+  let userinfo = window.sessionStorage.getItem('userinfo')
+  // 如果没有token切访问不是登录页面
+  if (!userinfo && to.path !== '/login') {
+    // 强制跳转登录界面
+    return next('/login')
+  }
+  next()
+})
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="login-container">
     <div class="login-box">
-      <el-form ref="loginFormRef" :model="loginForm" :rules='loginFormRules'>
-        <img src="./logo_index.png"/>
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules">
+        <img src="./logo_index.png" />
         <!-- prop使得校验规则可以找到当前目录进行匹配校验，值 就是当前项目的名称 -->
-        <el-form-item prop='moblie'>
-          <el-input v-model="loginForm.moblie" placeholder="请输入手机号码"></el-input>
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号码"></el-input>
         </el-form-item>
-        <el-form-item prop='code'>
+        <el-form-item prop="code">
           <el-input v-model="loginForm.code" placeholder="请输入验证码"></el-input>
         </el-form-item>
-        <el-form-item style="text-align:left;">
-           <!-- 复选框，单个复选框直接设置v-model即可，接收true/false的信息，表示是否选中 -->
+        <el-form-item style="text-align:left;" prop="xieyi">
+          <!-- 复选框，单个复选框直接设置v-model即可，接收true/false的信息，表示是否选中 -->
           <el-checkbox v-model="loginForm.xieyi" style="margin-right:10px;"></el-checkbox>
           <span>
             我已阅读并同意
@@ -31,28 +31,64 @@
 <script>
 export default {
   data () {
+    // 声明局部函数实现校验
+    let xieyiTest = function (rule, value, callback) {
+      // rule校验规则一般没有,value被校验的值,callback回调函数
+      // if (value) {
+      //   callback()
+      // } else {
+      //   callback(new Error('请无条件遵守规矩'))
+      // }
+      value ? callback() : callback(new Error('请无条件遵守规矩'))
+    }
     return {
       loginForm: {
-        moblie: '', // 手机号
-        code: '', // 验证码
-        xieyi: false // 协议
+        mobile: '13011112222', // 手机号
+        code: '246810', // 验证码
+        xieyi: true // 协议
       },
       // 给form表单域制作校验规则
       loginFormRules: {
-        moblie: [
+        // 项目名称:[{规则},{规则}] required为true项目必填,message错误提示
+        // min信息最小限制,max信息最大限制 pattern正则
+        mobile: [
           { required: true, message: '手机号码必填' },
           { pattern: /^1[35789]\d{9}$/, message: '手机号码格式不对' }
         ],
-        code: [
-          { required: true, message: '验证码必填' }
-        ]
+        code: [{ required: true, message: '验证码必填' }],
+        xieyi: [{ validator: xieyiTest }] // 自定义校验,协议validator
       }
     }
   },
   methods: {
     login () {
-      // name属性实现编程式导航
-      this.$router.push({ name: 'home' })
+      // 表单验证登录
+      // el-form表单对象.validate
+      // console.log(this) this.$refs.ref值为该表单组件对象
+      this.$refs.loginFormRef.validate(valid => {
+        // console.log(valid)校验通过true,不通过为false
+        if (!valid) {
+          return false
+        }
+        // 发送请求验证服务器端账号真实校验
+        let pro = this.$http({
+          url: '/mp/v1_0/authorizations',
+          method: 'POST',
+          data: this.loginForm
+        })
+        pro
+          .then(res => {
+            // console.log(res)
+            // 进入后台系统之前保存token,判断是否处于登录状态
+            window.sessionStorage.setItem('userinfo', JSON.stringify(res.data.data))
+            // name属性实现编程式导航
+            this.$router.push({ name: 'home' })
+          })
+          .catch(err => {
+            this.$message.error('手机号码或验证码错误:' + err)
+            // console.log(err)
+          })
+      })
     }
   }
 }
